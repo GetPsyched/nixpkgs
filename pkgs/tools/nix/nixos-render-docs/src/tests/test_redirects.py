75599adbd80c4df978e8e6db7ea07ecdcebeab93
GetPsyched <priyanshu@getpsyched.dev>
2024-09-20 19:00:23 +0530

    feat: initial set of unit tests

---
+++ pkgs/tools/nix/nixos-render-docs/src/tests/test_redirects.py
@@ -0,0 +1,33 @@
+import unittest
+from pathlib import Path
+
+from nixos_render_docs.manual import HTMLConverter, HTMLParameters
+
+
+class TestRedirects(unittest.TestCase):
+    def setUp(self):
+        self.md = HTMLConverter("1.0.0", HTMLParameters("", [], [], 2, 2, 2, Path("")), {})
+
+        with open(Path(__file__).parent / 'index.md', 'w') as infile:
+            infile.write("""
+# title {#title}
+
+## subtitle {#subtitle}
+            """)
+        self.md.convert(Path(__file__).parent / 'index.md', Path(__file__).parent / 'index.html')
+
+
+    def test_parsing_passes(self):
+        try:
+            self.md.parse_redirects({'title': [], 'subtitle': []}, Path(__file__).parent)
+        except Exception as error:
+            self.fail(f"redirect parsing failed raised an unexpected exception: {error}")
+
+    def test_missing_redirect_for_identifier(self):
+        with self.assertRaisesRegex(RuntimeError, "^following identifiers don't have a redirect: {'title', 'subtitle'}$"):
+            self.md.parse_redirects({}, Path(__file__).parent)
+
+
+    def test_orphan_identifier_in_redirects(self):
+        with self.assertRaisesRegex(RuntimeError, "^following identifiers missing in source: {'foo'}$"):
+            self.md.parse_redirects({'foo': ['index.html#title']}, Path(__file__).parent)
